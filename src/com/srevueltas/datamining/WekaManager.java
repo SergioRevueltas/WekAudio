533e5665fe6d7e3611d632e8a02ae0bb829265a7
sergio <sergiorevueltasestrada@gmail.com>
2014-08-18 18:08:07 +0200

    Classify method implemented
    - Implement 2 test case of classify

--- src/com/srevueltas/datamining/WekaManager.java
+++ src/com/srevueltas/datamining/WekaManager.java
@@ -1,18 +1,22 @@
 package com.srevueltas.datamining;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.ObjectInputStream;
 
-import weka.classifiers.bayes.BayesNet;
+import weka.classifiers.Classifier;
+import weka.core.DenseInstance;
+import weka.core.Instance;
 import weka.core.Instances;
-
+import weka.core.Utils;
 
 public class WekaManager {
-	
+
 	private String arffPath;
 	private String modelPath;
 	private String[] attributes;
-	
+
 	public WekaManager(String arffPath, String modelPath, String[] attributes) {
 		this.arffPath = arffPath;
 		this.modelPath = modelPath;
@@ -21,38 +25,55 @@ public class WekaManager {
 
 	/**
 	 * amb_A, amb_B, amb_C
-	 * @param train into exportedFeatureValues folder 
-	 * @param args 
+	 * 
+	 * @param trainSetName into exportedFeatureValues folder
+	 * @param at attributes of the new instance which is going to be classified
 	 * @return weka class value or -1 if error
 	 * @throws Exception
 	 */
-	public static String classify(String train, double[] at) throws Exception{
-		String arffPath = "exportedFeatureValues/" + train + ".arff"; 
-		String modelPath = "exportedFeatureValues/" + train + ".model"; 
-		BufferedReader br = new BufferedReader(new FileReader(arffPath));
-		Instances instancias = new Instances(br);
-		br.close();
-		if(instancias.numAttributes()-1 != at.length){
-			return "-1";
-		}
-		if (instancias.classIndex()==-1)
-			instancias.setClassIndex(instancias.numAttributes()-1);
-		for (int i = 0; i < instancias.numAttributes()-1; i++){
-			instancias.instance(0).setValue(i, at[i]);
-		}
-		double classValue = -1;
-		BayesNet bayesNet = (BayesNet) weka.core.SerializationHelper.read(modelPath);
-		//bayesNet.buildClassifier(instancias);
-		//MultilayerPerceptron redNeuronal = (MultilayerPerceptron) weka.core.SerializationHelper.read(modelPath);
+	public static String classify(String trainSetName, double[] at) throws Exception {
+		String arffPath = "exportedFeatureValues/" + trainSetName + ".arff";
+		String modelPath = "exportedFeatureValues/" + trainSetName + ".model";
+		String classLabel = "-1";
 		try {
-			classValue = bayesNet.classifyInstance(instancias.instance(0));			
+			// Read instances of train
+			BufferedReader br = new BufferedReader(new FileReader(arffPath));
+			Instances instancias = new Instances(br);
+			br.close();
+			if (instancias.numAttributes() - 1 != at.length) {
+				return "-1";
+			}
+			// set class index
+			if (instancias.classIndex() == -1)
+				instancias.setClassIndex(instancias.numAttributes() - 1);
+
+			// Instance instanceToClassify = (Instance) instancias.instance(instancias.numInstances()-1).copy();
+
+			// Empty instance sets weight to one, all values to be missing, and the reference to the dataset to null.
+			Instance instanceToClassify = (Instance) new DenseInstance(instancias.numAttributes());
+			for (int i = 0; i < instancias.numAttributes() - 1; i++) {
+				instanceToClassify.setValue(i, at[i]);
+			}
+			// set dataset
+			instanceToClassify.setDataset(instancias);
+			// optional because of DenseInstance initialization
+			instanceToClassify.setClassValue(Utils.missingValue());
+			// deserialize model
+			Classifier cls = null;
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(modelPath));
+			cls = (Classifier) ois.readObject();
+			ois.close();
+			// build classifier
+			cls.buildClassifier(instancias);
+			// classify
+			double classValue = -1;
+			classValue = cls.classifyInstance(instanceToClassify);
+			classLabel = instancias.classAttribute().value((int) classValue);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return new String(instancias.classAttribute().value((int)classValue));
-		
+		return classLabel;
+
 	}
-	
-	
-	
+
 }
--- src/com/srevueltas/test/TestWekaManager.java
+++ src/com/srevueltas/test/TestWekaManager.java
@@ -1,5 +1,7 @@
 package com.srevueltas.test;
 
+import junit.framework.Assert;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,27 +20,29 @@ public class TestWekaManager {
 	}
 
 	@Test
-	public void test_amb_A() {
-		String result = "";
-		double[] at = {2.713E-2,3.097E3,1.176E2,-1.311E2,-1.084E1,-5.714E0,-9.473E-1,4.177E0,3.388E0,1.239E0,2.714E0,-2.183E-1,-1.802E0,1.921E-1,7.793E-1,6.329E-1,3.972E-1,3.298E-1,-5.625E-1,-8.92E-1,-7.487E-1,-9.691E-1,5.735E-1,-1.552E-2,8.422E-1,-8.074E-1,1.232E-13,8.074E-1,-8.422E-1,1.552E-2,-5.735E-1,9.691E-1,7.487E-1,-9.136E-1,9.143E-1,-1.244E-1,3.429E-1,3.254E-1,3.609E-1,2.399E-1,1.014E-1,1.677E-1,0E0};
+	public void when_classify_an_existing_instance_of_amb_A_then_return_class_name() {
+		String trainSetName = "AB_DBA";
+		String result = "-";
+		double[] at = {5.371E-3,3.096E3,1.342E2,-1.264E2,-3.086E0,-1.114E0,2.505E0,3.611E0,3.268E0,4.627E-1,2.2E0,-3.061E-1,-1.111E0,9.093E-1,1.135E0,-3.019E-1,-2.636E-1,1.226E0,-5.798E-1,-1.868E-1,4.008E-1,-1.838E-1,3.085E-1,1.316E0,7.449E-1,-5.157E-1,1.354E-13,5.157E-1,-7.449E-1,-1.316E0,-3.085E-1,1.838E-1,-4.008E-1,-9.015E-1,8.16E-1,-1.054E-1,3.355E-1,1.889E-1,1.652E-1,5.63E-2,-8.396E-2,-1.29E-1,0E0};
 		try {
-			result = WekaManager.classify("AB_DBA", at);
+			result = WekaManager.classify(trainSetName, at);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("RESULT: " + result);
+		Assert.assertEquals("amb_A", result);
 	}
 	
 	@Test
-	public void test_amb_B() {
+	public void when_classify_an_existing_instance_of_amb_B_then_return_class_name() {
+		String trainSetName = "AB_DBA";
 		String result = "-";
 		double[] at = {5.86E-3,2.86E3,2.602E2,-1.05E2,-2.363E1,-5.589E0,-5.048E-1,1.996E0,4.998E0,-2.995E0,-3.598E-1,5.587E-1,3.42E-1,-3.808E-1,-7.219E-1,6.891E-1,2.265E0,1.473E0,5.411E-2,1.83E-2,1.225E0,3.574E-1,7.041E-1,1.569E0,5.899E-1,2.571E-1,1.035E-13,-2.571E-1,-5.899E-1,-1.569E0,-7.041E-1,-3.574E-1,-1.225E0,-6.879E-1,7.075E-1,-2.219E-1,2.762E-1,-1.415E-1,4.509E-1,1.469E-2,1.609E-1,1.938E-1,0E0};
 		try {
-			result = WekaManager.classify("AB_DBA", at);
+			result = WekaManager.classify(trainSetName, at);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("RESULT: " + result);
+		Assert.assertEquals("amb_B", result);
 	}
 
 }
