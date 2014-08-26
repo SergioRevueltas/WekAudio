e16b90392f77374bcd6756ed0f950f9773218233
sergio <sergiorevueltasestrada@gmail.com>
2014-08-26 20:08:49 +0200

    Fix bug related to add same aggregator many times

--- src/jAudioFeatureExtractor/AggregatorFrame.java
+++ src/jAudioFeatureExtractor/AggregatorFrame.java
@@ -35,39 +35,27 @@ import com.srevueltas.gui.CustomJTable;
 public class AggregatorFrame extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
-
 	private JPanel jContentPane = null;
-
-	private JScrollPane ActiveAggList = null;
-	
-	private CustomJTable ActiveAggTable = null;
-
-	private JScrollPane AggList = null;
-	
-	private CustomJTable AggListTable = null;
-
-	private JPanel AggButtonPanel = null;
-
-	private CustomJButton AggAdd = null;
-
-	private CustomJButton AggRemove = null;
-	
+	private JScrollPane activeAggList = null;
+	private CustomJTable activeAggTable = null;
+	private JScrollPane aggList = null;
+	private CustomJTable aggListTable = null;
+	private JPanel aggButtonPanel = null;
+	private CustomJButton aggAdd = null;
+	private CustomJButton aggRemove = null;
 	private AggEditorFrame aggEditorFrame = null;
-	
 	private Controller controller;
-
-	private CustomJButton DoneButton = null;
-
-	private CustomJButton Abort = null;
-	private CustomJLabel lblCurrentAggregators;
-	private CustomJLabel lblAggregatorsList;
+	private CustomJButton doneButton = null;
+	private CustomJButton abort = null;
+	private CustomJLabel lblCurrentAggregators = null;
+	private CustomJLabel lblAggregatorsList = null;
 
 	/**
 	 * This is the default constructor
 	 */
 	public AggregatorFrame(Controller c) {
 		super();
-		
+
 		controller = c;
 		initialize();
 	}
@@ -92,7 +80,8 @@ public class AggregatorFrame extends JFrame implements ActionListener {
 	private JPanel getJContentPane() {
 		if (jContentPane == null) {
 			jContentPane = new JPanel();
-			jContentPane.setLayout(new MigLayout("", "[245.00px:452px][80.00:39.00][80.00px:32px][300.00:n]", "[][198.00px:462px:185.00px][::34.00]"));
+			jContentPane.setLayout(new MigLayout("", "[245.00px:452px][80.00:39.00][80.00px:32px][300.00:n]",
+					"[][198.00px:462px:185.00px][::34.00]"));
 			jContentPane.add(getLblCurrentAggregators(), "cell 0 0");
 			jContentPane.add(getLblAggregatorsList(), "cell 3 0");
 			jContentPane.add(getActiveAggList(), "cell 0 1,alignx left,aligny bottom");
@@ -104,197 +93,214 @@ public class AggregatorFrame extends JFrame implements ActionListener {
 		}
 		return jContentPane;
 	}
-	
 
 	/**
-	 * This method initializes ActiveAggList	
-	 * 	
-	 * @return javax.swing.JScrollPane	
+	 * This method initializes ActiveAggList
+	 * 
+	 * @return javax.swing.JScrollPane
 	 */
 	private JScrollPane getActiveAggList() {
-		if (ActiveAggList == null) {
-			ActiveAggList = new JScrollPane();
-			ActiveAggList.setViewportView(getActiveAggTable());
-			ActiveAggList.setBackground(OuterFrame.GRAY);
-			ActiveAggList.getViewport().setBackground(OuterFrame.GRAY);
+		if (activeAggList == null) {
+			activeAggList = new JScrollPane();
+			activeAggList.setViewportView(getActiveAggTable());
+			activeAggList.setBackground(OuterFrame.GRAY);
+			activeAggList.getViewport().setBackground(OuterFrame.GRAY);
 		}
-		return ActiveAggList;
+		return activeAggList;
 	}
 
 	/**
-	 * This method initializes ActiveAggTable	
-	 * 	
-	 * @return javax.swing.JTable	
+	 * This method initializes ActiveAggTable
+	 * 
+	 * @return javax.swing.JTable
 	 */
 	private JTable getActiveAggTable() {
-		if (ActiveAggTable == null) {
-			ActiveAggTable = new CustomJTable();
+		if (activeAggTable == null) {
+			activeAggTable = new CustomJTable();
 			controller.activeAgg_ = new ActiveAggTableModel();
 			controller.activeAgg_.init(controller);
-			ActiveAggTable.setModel(controller.activeAgg_);
-			ActiveAggTable.addMouseListener(new java.awt.event.MouseAdapter() {
+			activeAggTable.setModel(controller.activeAgg_);
+			activeAggTable.addMouseListener(new java.awt.event.MouseAdapter() {
+
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					if(e.getClickCount()==2){
-						int row = ActiveAggTable.getSelectedRow();
-						if(row>=0){
-							aggEditorFrame = new AggEditorFrame((jAudioFeatureExtractor.Aggregators.Aggregator)controller.activeAgg_.getAggregator(row),controller);
+					if (e.getClickCount() == 2) {
+						int row = activeAggTable.getSelectedRow();
+						if (row >= 0) {
+							aggEditorFrame =
+									new AggEditorFrame(
+											(jAudioFeatureExtractor.Aggregators.Aggregator) controller.activeAgg_
+													.getAggregator(row), controller);
 							aggEditorFrame.setVisible(true);
-							((ActiveAggTableModel)ActiveAggTable.getModel()).setAggregator(row, aggEditorFrame.getAggregator(), aggEditorFrame.isEdited());			
+							((ActiveAggTableModel) activeAggTable.getModel()).setAggregator(row, aggEditorFrame
+									.getAggregator(), aggEditorFrame.isEdited());
 						}
 					}
 				}
 			});
 		}
-		return ActiveAggTable;
+		return activeAggTable;
 	}
 
 	/**
-	 * This method initializes AggList	
-	 * 	
-	 * @return javax.swing.JScrollPane	
+	 * This method initializes AggList
+	 * 
+	 * @return javax.swing.JScrollPane
 	 */
 	private JScrollPane getAggList() {
-		if (AggList == null) {
-			AggList = new JScrollPane();
-			AggList.setViewportView(getAggListTable());
-			AggList.setBackground(OuterFrame.GRAY);
-			AggList.getViewport().setBackground(OuterFrame.GRAY);
+		if (aggList == null) {
+			aggList = new JScrollPane();
+			aggList.setViewportView(getAggListTable());
+			aggList.setBackground(OuterFrame.GRAY);
+			aggList.getViewport().setBackground(OuterFrame.GRAY);
 		}
-		return AggList;
+		return aggList;
 	}
 
 	/**
-	 * This method initializes AggListTable	
-	 * 	
-	 * @return javax.swing.JTable	
+	 * This method initializes AggListTable
+	 * 
+	 * @return javax.swing.JTable
 	 */
 	private JTable getAggListTable() {
-		if (AggListTable == null) {
-			AggListTable = new CustomJTable();
+		if (aggListTable == null) {
+			aggListTable = new CustomJTable();
 			controller.aggList_ = new AggListTableModel();
 			controller.aggList_.init(controller.dm_.aggregatorMap);
-			AggListTable.setModel(controller.aggList_);
-			AggListTable.getColumnModel().getColumn(0).setPreferredWidth(60);
-			AggListTable.getColumnModel().getColumn(0).setMaxWidth(60);
-			AggListTable.getColumnModel().getColumn(1).setPreferredWidth(150);
-			AggListTable.addMouseListener(new java.awt.event.MouseAdapter() {
+			aggListTable.setModel(controller.aggList_);
+			aggListTable.getColumnModel().getColumn(0).setPreferredWidth(60);
+			aggListTable.getColumnModel().getColumn(0).setMaxWidth(60);
+			aggListTable.getColumnModel().getColumn(1).setPreferredWidth(150);
+			aggListTable.addMouseListener(new java.awt.event.MouseAdapter() {
+
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					if(e.getClickCount()==2){
-						int[] row = AggListTable.getSelectedRows();
-						for(int i=0;i<row.length;++i){
-							AggListTableModel list = (AggListTableModel)AggListTable.getModel();
+					if (e.getClickCount() == 2) {
+						int[] row = aggListTable.getSelectedRows();
+						for (int i = 0; i < row.length; ++i) {
+							AggListTableModel list = (AggListTableModel) aggListTable.getModel();
 							Aggregator prototype = list.getAggregator(row[i]);
-							Aggregator newAgg = (Aggregator)prototype.clone();
-							((ActiveAggTableModel)ActiveAggTable.getModel()).addAggregator(newAgg);							
+							Aggregator newAgg = (Aggregator) prototype.clone();
+							((ActiveAggTableModel) activeAggTable.getModel()).addAggregator(newAgg);
 						}
 					}
 				}
 			});
 		}
-		return AggListTable;
+		return aggListTable;
 	}
 
 	/**
-	 * This method initializes AggButtonPanel	
-	 * 	
-	 * @return javax.swing.JPanel	
+	 * This method initializes AggButtonPanel
+	 * 
+	 * @return javax.swing.JPanel
 	 */
 	private JPanel getAggButtonPanel() {
-		if (AggButtonPanel == null) {
-			AggButtonPanel = new JPanel();
-			AggButtonPanel.setLayout(new MigLayout("", "[144.00px:59px:150.00px]", "[25px][]"));
-			AggButtonPanel.add(getAggAdd(), "cell 0 0,growx,aligny top");
-			AggButtonPanel.setBackground(OuterFrame.BLACK_BACKGROUND);
-			AggButtonPanel.add(getAggRemove(), "cell 0 1,growx,aligny top");
+		if (aggButtonPanel == null) {
+			aggButtonPanel = new JPanel();
+			aggButtonPanel.setLayout(new MigLayout("", "[144.00px:59px:150.00px]", "[25px][]"));
+			aggButtonPanel.add(getAggAdd(), "cell 0 0,growx,aligny top");
+			aggButtonPanel.setBackground(OuterFrame.BLACK_BACKGROUND);
+			aggButtonPanel.add(getAggRemove(), "cell 0 1,growx,aligny top");
 		}
-		return AggButtonPanel;
+		return aggButtonPanel;
 	}
 
 	/**
-	 * This method initializes AggAdd	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes AggAdd
+	 * 
+	 * @return javax.swing.JButton
 	 */
 	private JButton getAggAdd() {
-		if (AggAdd == null) {
-			AggAdd = new CustomJButton("Add");
-			AggAdd.setToolTipText("Add a new Aggregator to be applied");
-			AggAdd.addActionListener(new java.awt.event.ActionListener() {
-				public void actionPerformed(java.awt.event.ActionEvent e) {
-					int row = AggListTable.getSelectedRow();
-					if(row != -1){
-						AggListTableModel list = (AggListTableModel)AggListTable.getModel();
-						Aggregator prototype = list.getAggregator(row);
-						Aggregator newAgg = (Aggregator)prototype.clone();
-						((ActiveAggTableModel)ActiveAggTable.getModel()).addAggregator(newAgg);
-					}
-					System.out.println("actionPerformed()"); 				}
-			});
-		}
-		return AggAdd;
-	}
+		if (aggAdd == null) {
+			aggAdd = new CustomJButton("Add");
+			aggAdd.setToolTipText("Add a new Aggregator to be applied");
+			aggAdd.addActionListener(new java.awt.event.ActionListener() {
 
-	/**
-	 * This method initializes AggRemove	
-	 * 	
-	 * @return javax.swing.JButton	
-	 */
-	private JButton getAggRemove() {
-		if (AggRemove == null) {
-			AggRemove = new CustomJButton("Remove");
-			AggRemove.setToolTipText("Remove an aggregator that has been previously defined");
-			AggRemove.addActionListener(new java.awt.event.ActionListener() {
 				public void actionPerformed(java.awt.event.ActionEvent e) {
-					int row = ActiveAggTable.getSelectedRow();
-					if(row >= 0){
-						((ActiveAggTableModel)ActiveAggTable.getModel()).removeAggregator(row);
+					int selectedRow = aggListTable.getSelectedRow();
+					if (selectedRow != -1) {
+						AggListTableModel list = (AggListTableModel) aggListTable.getModel();
+						Aggregator prototype = list.getAggregator(selectedRow);
+						String name = prototype.getAggregatorDefinition().name;
+						boolean isAlreadySelected = false;
+						for (int row = 0; row < activeAggTable.getRowCount(); row++) {
+							if (name.equals(activeAggTable.getValueAt(row, 0))) {
+								isAlreadySelected = true;
+								break;
+							}
+						}
+						if (!isAlreadySelected){
+							Aggregator newAgg = (Aggregator) prototype.clone();
+							((ActiveAggTableModel) activeAggTable.getModel()).addAggregator(newAgg);
+						}
 					}
 				}
 			});
 		}
-		return AggRemove;
+		return aggAdd;
 	}
 
 	/**
-	 * This method initializes DoneButton	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes AggRemove
+	 * 
+	 * @return javax.swing.JButton
+	 */
+	private JButton getAggRemove() {
+		if (aggRemove == null) {
+			aggRemove = new CustomJButton("Remove");
+			aggRemove.setToolTipText("Remove an aggregator that has been previously defined");
+			aggRemove.addActionListener(new java.awt.event.ActionListener() {
+
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					int row = activeAggTable.getSelectedRow();
+					if (row >= 0) {
+						((ActiveAggTableModel) activeAggTable.getModel()).removeAggregator(row);
+					}
+				}
+			});
+		}
+		return aggRemove;
+	}
+
+	/**
+	 * This method initializes DoneButton
+	 * 
+	 * @return javax.swing.JButton
 	 */
 	private JButton getDoneButton() {
-		if (DoneButton == null) {
-			DoneButton = new CustomJButton("Save");
-			DoneButton.setToolTipText("Save and exit aggregator editing.");
-			DoneButton.addActionListener(this);
+		if (doneButton == null) {
+			doneButton = new CustomJButton("Save");
+			doneButton.setToolTipText("Save and exit aggregator editing.");
+			doneButton.addActionListener(this);
 		}
-		return DoneButton;
+		return doneButton;
 	}
 
 	/**
-	 * This method initializes Abort	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes Abort
+	 * 
+	 * @return javax.swing.JButton
 	 */
 	private JButton getAbort() {
-		if (Abort == null) {
-			Abort = new CustomJButton("Cancel");
-			Abort.setToolTipText("Exit without saving.");
-			Abort.addActionListener(this);
+		if (abort == null) {
+			abort = new CustomJButton("Cancel");
+			abort.setToolTipText("Exit without saving.");
+			abort.addActionListener(this);
 		}
-		return Abort;
+		return abort;
 	}
-	
+
 	/**
 	 * handles events on this window
 	 */
 	public void actionPerformed(ActionEvent event) {
-		if(event.getSource() == DoneButton){
-			if((controller.activeAgg_.getAggregator() !=null)&&(controller.activeAgg_.getAggregator().length>0)){
+		if (event.getSource() == doneButton) {
+			if ((controller.activeAgg_.getAggregator() != null) && (controller.activeAgg_.getAggregator().length > 0)) {
 				controller.dm_.aggregators = controller.activeAgg_.getAggregator();
 				this.setVisible(false);
-			}else{
-				controller.dm_.aggregators = new Aggregator[]{new Mean()};
+			} else {
+				controller.dm_.aggregators = new Aggregator[] { new Mean() };
 			}
-		}else if(event.getSource() == Abort){
+		} else if (event.getSource() == abort) {
 			this.setVisible(false);
 		}
 	}
@@ -306,6 +312,7 @@ public class AggregatorFrame extends JFrame implements ActionListener {
 		}
 		return lblCurrentAggregators;
 	}
+
 	private JLabel getLblAggregatorsList() {
 		if (lblAggregatorsList == null) {
 			lblAggregatorsList = new CustomJLabel("Aggregators list:");
