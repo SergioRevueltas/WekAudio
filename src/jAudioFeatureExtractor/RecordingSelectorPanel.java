ab5e7ae805fa42843e1dddbb8703d5c5e8e62f86
sergio <sergiorevueltasestrada@gmail.com>
2014-08-26 20:37:28 +0200

    Fix number appearance of recordings jtable

--- src/jAudioFeatureExtractor/RecordingSelectorPanel.java
+++ src/jAudioFeatureExtractor/RecordingSelectorPanel.java
@@ -198,7 +198,7 @@ public class RecordingSelectorPanel extends JPanel implements ActionListener {
 
 		recordings_table = new CustomJTable(controller.rtm_);
 		
-		recordings_table.getColumnModel().getColumn(0).setPreferredWidth(25);
+		recordings_table.getColumnModel().getColumn(0).setPreferredWidth(35);
 		recordings_table.getColumnModel().getColumn(1).setPreferredWidth(150);
 		recordings_table.getColumnModel().getColumn(2).setPreferredWidth(400);
 		
