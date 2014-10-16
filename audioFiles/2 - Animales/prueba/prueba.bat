b7d39c0a160c263d5ea7a6df99ad6868f718b6f6
sergio <sergiorevueltasestrada@gmail.com>
2014-10-16 18:47:57 +0200

    Change gitignore
    +.bat *.zip *.model

--- audioFiles/2 - Animales/prueba/prueba.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-SETLOCAL ENABLEDELAYEDEXPANSION
-SET old=)
-SET new=\"
-for /f "tokens=*" %%f in ('dir /b *.wav') do (
-  SET newname=%%f
-  SET newname=!newname:%old%=%new%!
-  move "%%f" "!newname!"
-)
